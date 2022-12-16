6fdf332d3fbb8b6458e2dc96fd6baf4dc5454c85
Rudolfinio <rafi321112@gmail.com>
2022-12-16 16:59:35 +0100

    konfiguracja

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$config = new \App\Service\Config();
+
+$templating = new \App\Service\Templating();
+$router = new \App\Service\Router();
+
+$action = $_REQUEST['action'] ?? null;
+switch ($action) {
+    case 'post-index':
+    case null:
+        $controller = new \App\Controller\PostController();
+        $view = $controller->indexAction($templating, $router);
+        break;
+    case 'post-create':
+        $controller = new \App\Controller\PostController();
+        $view = $controller->createAction($_REQUEST['post'] ?? null, $templating, $router);
+        break;
+    case 'post-edit':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\PostController();
+        $view = $controller->editAction($_REQUEST['id'], $_REQUEST['post'] ?? null, $templating, $router);
+        break;
+    case 'post-show':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\PostController();
+        $view = $controller->showAction($_REQUEST['id'], $templating, $router);
+        break;
+    case 'post-delete':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\PostController();
+        $view = $controller->deleteAction($_REQUEST['id'], $router);
+        break;
+    default:
+        $view = 'Not found';
+        break;
+}
+
+if ($view) {
+    echo $view;
+}
---
+++ templates/nav.html.php
@@ -0,0 +1,9 @@
+<?php
+/** @var $router \App\Service\Router */
+
+?>
+<ul>
+    <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
+    <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
+</ul>
+<?php

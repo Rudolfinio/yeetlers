98930a773745433971f562db4622e07a14072349
Rudolfinio <rafi321112@gmail.com>
2023-01-05 16:29:26 +0100

    budynki update

--- templates/budynek/_form.html.php
+++ templates/budynek/_form.html.php
@@ -17,17 +17,17 @@
 </div>
 
 <div class="form-group">
-    <label for="kod_pocztowy">miasto</label>
+    <label for="kod_pocztowy">kod pocztowy</label>
     <input type="text" id="kod_pocztowy" name="budynek[kod_pocztowy]" value="<?= $post ? $post->getKodPocztowy() : '' ?>">
 </div>
 
 <div class="form-group">
-    <label for="ulica">miasto</label>
+    <label for="ulica">ulica</label>
     <input type="text" id="ulica" name="budynek[ulica]" value="<?= $post ? $post->getUlica() : '' ?>">
 </div>
 
 <div class="form-group">
-    <label for="nr_budynku">miasto</label>
+    <label for="nr_budynku">nr budynku</label>
     <input type="text" id="nr_budynku" name="budynek[nr_budynku]" value="<?= $post ? $post->getNrBudynku(): '' ?>">
 </div>
 
